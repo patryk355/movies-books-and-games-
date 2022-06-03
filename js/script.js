875eeed4b417ff80500389ee5d4d6dceb1867687
Patryk <patrykkarasinski123@wp.pl>
2022-06-03 19:50:03 +0200

    add replace method on href

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 const tiles = document.querySelectorAll('.tile');
 tiles.forEach((tile) => {
   tile.addEventListener('click', () => {
-    window.location.href = `../subpages/${tile.id}.html`;
+    // window.location.href = `../subpages/${tile.id}.html`;
+    window.location.href = window.location.href.replace(
+      'index',
+      `subpages/${tile.id}`
+    );
   });
 });
 
